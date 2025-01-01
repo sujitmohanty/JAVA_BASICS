4f2cc472f4d2c24e5cd11e9f222651e5cab61bc7
Sujit Mohanty <sujit.mty@outlook.com>
2025-01-01 23:56:27 +0100

    Third commit

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args){
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 11);
+    }
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args){
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
